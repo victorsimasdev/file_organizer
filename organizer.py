76cac2ba879133ff7b2902ee17dc3e82d9bacca7
Victor Simas <victorsimasdev@gmail.com>
2024-08-02 11:26:39 -0300

    feat: Adicionadas verificações adicionais por segurança, adicionado verificação de conteúdo caso já exista uma arquivo com memso nome.

--- organizer.py
+++ organizer.py
@@ -1,27 +1,39 @@
 import os
 import shutil
+import filecmp
 
-path = input('Insira o caminho para organizar: ')
-existing_files = 0
-files = os.listdir(path)
-try:
-    for file in files:
-        filename, extension = os.path.splitext(file)
-        extension = extension[1:]
+def organize_files(path):
+    existing_files = 0
+    files = os.listdir(path)
+    try:
+        for file in files:
+            file_path = os.path.join(path, file)
+            if os.path.isdir(file_path):
+                continue
 
-        if os.path.exists(path+'/'+extension):
-            if not os.path.exists(path+'/'+extension+'/'+file):
-                shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
+            filename, extension = os.path.splitext(file)
+            extension = extension[1:]
+            extension_dir = os.path.join(path, extension)
+            
+            if not os.path.exists(extension_dir):
+                os.makedirs(extension_dir)
+
+            new_file_path = os.path.join(extension_dir, file)
+            if not os.path.exists(new_file_path):
+                shutil.move(file_path, new_file_path)
             else:
-                existing_files += 1
-        else:
-            os.makedirs(path+'/'+extension)
-            if not os.path.exists(path+'/'+extension+'/'+file):
-                shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
-            else:
-                existing_files += 1
-    print('Arquivos organizados com sucesso!')
-    if existing_files != 0:
-        print(f'{existing_files} arquivos já existentes não foram movidos.')
-except Exception as e:
-    print('Erro ao organizar arquivos: ', e)
+                if not filecmp.cmp(file_path, new_file_path, shallow=False):
+                    new_file_path = os.path.join(extension_dir, f"copy_{file}")
+                    shutil.move(file_path, new_file_path)
+                else:
+                    existing_files += 1
+
+        print('Arquivos organizados com sucesso!')
+        if existing_files != 0:
+            print(f'{existing_files} arquivos já existentes não foram movidos.')
+    except Exception as e:
+        print('Erro ao organizar arquivos: ', e)
+
+if __name__ == "__main__":
+    path = os.path.dirname(os.path.abspath(__file__))
+    organize_files(path)
